464c59548c79bf1daacf21c66c6df82d760e8f64
Anthony Koerber <akoerber@prophet.com>
2014-04-29 11:48:45 -0500

    added hotbox ui design to github

---
+++ development/js/consolelog.js
@@ -0,0 +1,16 @@
+// CONSOLE LOG - Log wrapper
+/******************************************************/
+var ConsoleLog = {
+	
+	log : function (something) {
+	
+		if (typeof console === "undefined") {
+			return;
+		} else {
+			console.log(something);
+		}
+		
+	}
+	
+};
+/******************************************************/
